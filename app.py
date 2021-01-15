718203cae5935700ff2d961e9d3e91d0732963b7
pranabsarkar <sarkarpranab66@gmail.com>
2021-01-16 01:51:44 +0530

    basic changes

--- app.py
+++ app.py
@@ -29,16 +29,19 @@ def home():
  
 @app.route('/login', methods=['POST'])
 def user_login():
-    session['logged_in'] = False
-    response = process_client.database_process.search_item_user_db(email_address=request.form['email address'])
-    if response['password'] == request.form['password'] and response['email_address'] == request.form['email address']:
-        session['logged_in'] = True
-    else:
-    	message="Please enter correct username and password!"
-        flash(message)
+    try:
         session['logged_in'] = False
-
-    return home()
+        response = process_client.database_process.search_item_user_db(email_address=request.form['email address'])
+        if response['password'] == request.form['password'] and response['email_address'] == request.form['email address']:
+            session['logged_in'] = True
+        else:
+            message="Please enter correct username and password!"
+            flash(message)
+            session['logged_in'] = False
+    except Exception as e:
+        print(e)
+    else:
+        return home()
 
 @app.route('/register', methods=['GET'])
 def register():
@@ -62,22 +65,26 @@ def logout():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    uploaded_files = request.files.getlist("file[]")
-    filenames = []
-    for file in uploaded_files:
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            list_files = os.listdir('uploads')
-            f_save=str(len(list_files)+1)+'.pdf'            
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_save))            
-            filenames.append(filename)
-            key_name='uploads/'+f_save
-            process_client.storage_process.upload_file(key_name,'pranab1',f_save)
-            process_client.database_process.insert_item_into_storage_db(
-                s3_key_name=key_name,
-                original_name=filename
-            )
-    return render_template('upload_file.html', filenames=filenames)
+    try:
+        uploaded_files = request.files.getlist("file[]")
+        filenames = []
+        for file in uploaded_files:
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                list_files = os.listdir('uploads')
+                f_save=str(len(list_files)+1)+'.pdf'            
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], f_save))            
+                filenames.append(filename)
+                key_name='uploads/'+f_save
+                process_client.storage_process.upload_file(key_name,'pranab1',f_save)
+                process_client.database_process.insert_item_into_storage_db(
+                    s3_key_name=key_name,
+                    original_name=filename
+                )
+    except Exception as e:
+        print(e)
+    else:
+        return render_template('upload_file.html', filenames=filenames)
 
 
 @app.route('/uploads/<filename>')
@@ -88,5 +95,8 @@ def uploaded_file(filename):
     )
  
 if __name__ == "__main__":
-    app.secret_key = os.urandom(12)
-    app.run(port=9000)
+    try:
+        app.secret_key = os.urandom(12)
+        app.run(debug=True, port=9000)
+    except Exception as e:
+        print(e)
